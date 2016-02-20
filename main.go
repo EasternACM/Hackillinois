8e2b7f2db38e063b0a2feeda4f9cc93940810f23
Zachary Collins <zacharyjacobcollins@gmail.com>
2016-02-20 11:40:29 -0500

    init

--- main.go
+++ main.go
@@ -2,11 +2,16 @@ package main
 import (
 	"net/http"
         "io"
+        "net/url"
 )
     
 func login(w http.ResponseWriter, r *http.Request) {
-  
-   io.WriteString(w, r.URL.String())
+   s := r.URL.String()
+   u, _ := url.Parse(s) 
+   m := u.Query()
+   i := m["code"]   
+   code := i[0]   
+   io.WriteString(w, code)
 }
 
 func main() {
