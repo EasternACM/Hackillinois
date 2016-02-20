1f7d5ea7179f8fdcc2ad221602b251bd93324aba
Zachary Collins <zacharyjacobcollins@gmail.com>
2016-02-20 15:53:36 -0500

    updates

--- main.go
+++ main.go
@@ -5,14 +5,24 @@ import (
 	"net/http"
 )
 
-func login(w http.ResponseWriter, r *http.Request) {
+func getCode(w http.ResponseWriter, r *http.Request) {
 	log.Print("hit")
 	code := r.URL.Query().Get("code")
 	log.Print(code)
+	sendCode(code)
+}
+
+func sendCode(code string) {
+	resp, err := http.Post("https://hackillinois.climate.com/api/oauth/token", "grant_type=\"authorization_code\" scope=\"openid user\" redirect_uri = zacc.xyz:8000 code = "+code, nil)
+	if err != nil {
+		panic(err)
+	}
+	log.Print(resp)
 }
 
 func main() {
 	log.Print("start")
-	http.HandleFunc("/", login)
+	http.HandleFunc("/", getCode)
 	http.ListenAndServe(":8000", nil)
+
 }
