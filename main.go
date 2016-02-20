a6f08a11eac832e3d228a6820c35240e2ed15107
Zachary Collins <zacharyjacobcollins@gmail.com>
2016-02-20 07:44:56 -0500

    updates

--- main.go
+++ main.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 )
 
 func login(w http.ResponseWriter, r *http.Request) {
-	fmt.Print("logged")
+	io.WriteString(w, "Hello world!")
+	fmt.Print("Hello World")
 }
 
 func main() {
 	http.HandleFunc("/login/", login)
-	http.ListenAndServe(":8000", nil)
+	http.ListenAndServe(":1337", nil)
 }
