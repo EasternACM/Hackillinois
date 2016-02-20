75ecc5e35bb4c2f74f85485aa6fba2290f2f0280
Zachary Collins <zacharyjacobcollins@gmail.com>
2016-02-20 11:58:23 -0500

    updates

--- main.go
+++ main.go
@@ -9,12 +9,12 @@ func login(w http.ResponseWriter, r *http.Request) {
    s := r.URL.String()
    u, _ := url.Parse(s) 
    m := u.Query()
-   i := m["code"]   
-   code := i[0]   
-   io.WriteString(w, code)
+//   i := m["code"]   
+  // code := i[0]   
+   io.WriteString(w, "code")
 }
 
 func main() {
-	http.HandleFunc("/login/", login)
+	http.HandleFunc("/login", login)
 	http.ListenAndServe(":1337", nil)
 }
